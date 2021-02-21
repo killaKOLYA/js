a5eb38c2392df4415287e090d1e7bbe051b51034
KOKA <mykola.dukhnych@gmail.com>
2021-02-21 20:41:54 +0200

    functions

--- js/script.js
+++ js/script.js
@@ -1,9 +1,8 @@
 'use strict';
 
-// 1.1
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+let numberOfFilms;
+start();
 
-// 1.2 
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -12,52 +11,91 @@ const personalMovieDB = {
     privat: false
 };
 
-if (personalMovieDB.count < 10) {
-    alert('Просмотрено довольно мало фильмов');
-} else if (personalMovieDB.count <= 30) {
-    alert('Вы классический зритель');
-} else if (personalMovieDB.count > 30) {
-    alert('Вы киноман');
-} else {
-    alert('Произошла ошибка');
-};
+// detectPersonalLevel();
+// remembermyFilms();
+// writeYourGenres(personalMovieDB.genres);
+showMyDB(personalMovieDB.privat);
 
-// 1.3; 2.1
-for (let i = 0; i<2; i++) { 
 
-    let filmName = prompt('Один из последних просмотренных фильмов?', ''),
-        filmRating = prompt('На сколько оцените его', '');
-    
-    if (filmName != null && filmRating != null && filmName != '' && filmRating != '' && filmName.length <10) {
-        personalMovieDB.movies[filmName] = filmRating;
-    } else {
-        alert('Ошибка');
-        --i;
+// Functions
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
     }
-    // let filmName, filmRating;
-    // do {
-    //     filmName = prompt('Один из последних просмотренных фильмов?', '');
-    //     if (filmName == null) {
-    //         alert('Нельзя отменить!');
-    //     } else if (filmName == '') {
-    //         alert('Введите название фильма!');
-    //     } else if (filmName.length > 10) {
-    //         alert('Название фильма не должно быть больше 50 символов');
-    //     } else {
-    //         do {
-    //             filmRating = prompt(`На сколько вы оцениваете фильм "${filmName}"`, '');
-    //             if (filmRating == null) {
-    //                 alert('Нельзя отменить!');
-    //             } else if (filmRating == '') {
-    //                 alert('Введите оценку для фильма!');
-    //             } else {
-    //                 personalMovieDB.movies[filmName] = filmRating;
-    //             }
-    //         } while (filmRating == null || filmRating == '');
-    //     }
-    // } while (filmName == null || filmName == '' || filmName.length >10);
-    
-    //personalMovieDB.movies[prompt('Один из последних просмотренных фильмов?', '')] = prompt('На сколько оцените его?', '');
+}
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        alert('Просмотрено довольно мало фильмов');
+    } else if (personalMovieDB.count <= 30) {
+        alert('Вы классический зритель');
+    } else if (personalMovieDB.count > 30) {
+        alert('Вы киноман');
+    } else {
+        alert('Произошла ошибка');
+    };
+}
+function remembermyFilms() {
+    for (let i = 0; i<2; i++) { 
+
+        // let filmName = prompt('Один из последних просмотренных фильмов?', ''),
+        //     filmRating = prompt('На сколько оцените его', '');
+        
+        // if (filmName != null && filmRating != null && filmName != '' && filmRating != '' && filmName.length <10) {
+        //     personalMovieDB.movies[filmName] = filmRating;
+        // } else {
+        //     alert('Ошибка');
+        //     --i;
+        // }
+
+        let filmName, filmRating;
+        do {
+            filmName = prompt('Один из последних просмотренных фильмов?', '');
+            if (filmName == null) {
+                alert('Нельзя отменить!');
+            } else if (filmName == '') {
+                alert('Введите название фильма!');
+            } else if (filmName.length > 10) {
+                alert('Название фильма не должно быть больше 50 символов');
+            } else {
+                do {
+                    filmRating = prompt(`На сколько вы оцениваете фильм "${filmName}"`, '');
+                    if (filmRating == null) {
+                        alert('Нельзя отменить!');
+                    } else if (filmRating == '') {
+                        alert('Введите оценку для фильма!');
+                    } else {
+                        personalMovieDB.movies[filmName] = filmRating;
+                    }
+                } while (filmRating == null || filmRating == '');
+            }
+        } while (filmName == null || filmName == '' || filmName.length >10);
+        
+        //personalMovieDB.movies[prompt('Один из последних просмотренных фильмов?', '')] = prompt('На сколько оцените его?', '');
+    }
+}
+function showMyDB(hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    } else {
+        console.log('Это приватный контент!');
+    }
+}
+function writeYourGenres(genres) {
+    for(let i = 0; i<3; i++) {
+        let  genre;
+        do {
+            genre = prompt(`Ваш любимый жанр под номером ${i+1}`,'');
+            if(genre == null) {
+                alert('Нельзя отменить!');
+            } else if(genre == '') {
+                alert(`Введите жанр фильма ${i+1}!`);
+            } else {
+                genres[i] = genre;
+            }
+        } while(genre == null || genre == '');
+    }
 }
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
